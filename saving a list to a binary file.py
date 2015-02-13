2e23a3375bf361ada2ea27dd7aa7c09617f5050c
l.travers55@gmail.com <44858@PC004142.coll.lr.local>
2015-02-13 14:29:26 +0000

    saving a list of records to a binary file

--- saving a list to a binary file.py
+++ saving a list to a binary file.py
@@ -1,6 +1,6 @@
 #Lewis Travers
 #13/2/2015
-#saving a list to a binary file
+#saving a list of records to a binary file
 
 import pickle
 
@@ -12,10 +12,15 @@ class People:
 names = []
 
 def details(names, People):
-    new_person = People()
-    new_person.name = input("Please enter a person's name: ")
-    new_person.DoB = input("Please enter their DoB in this format dd/mm/yyyy: ")
-    names.append(new_person)
+    done = False
+    while done == False:
+        new_person = People()
+        new_person.name = input("Please enter a person's name. Press only the enter key if you are finished: ")
+        if new_person.name == "":
+            done = True
+        else:
+            new_person.DoB = input("Please enter their DoB in this format dd/mm/yyyy: ")
+            names.append(new_person)
 
 def binary_file(names):
     with open("people.txt", mode="wb") as people_file:
